dc780549d4cb6bcacf7f9c90e282b4542d043150
baiyao <1272764421@qq.com>
2021-10-10 18:50:17 +0800

    框架搭建

---
+++ carplatform/src/test/java/com/example/carplatform/CarplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarplatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
